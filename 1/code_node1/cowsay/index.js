04fbb30bc65409e925b9ae79d30b388cd3a34faf
mhellar <mark.hellar@gmail.com>
2017-02-28 16:53:16 -0800

    upload new code

--- 1/code_node1/cowsay/index.js
+++
@@ -1,8 +0,0 @@
-var cowsay = require("cowsay");
-
-console.log(cowsay.think({
-    text : "I'm a moooodule",
-    e : "0O",
-    T : "{} ",
-    Mode: "Borg"
-}));
--- 1/code_node1/sync.js
+++
@@ -1,5 +0,0 @@
-// synchchronous code, code runs top to botttom
-var myNumber = 1
-function addOne() { myNumber++ } // define the function
-addOne() // run the function
-console.log(myNumber) // logs out 2
