d77a3fee614130f0ca36b33359858533ec82b2cd
Mark Hellar <mark.hellar@gmail.com>
2018-05-10 22:58:07 -0700

    fix

--- 2/code2/websocket_simple/app.js
+++ 2/code2/websocket_simple/app.js
@@ -3,41 +3,22 @@ var app = express();
 
 // Set up a URL route
 app.get("/", function(req, res) {
- res.send("Heroku Demo!");
+    res.sendFile(__dirname + '/index.html');
 });
 
 // bind the app to listen for connections on a specified port
-var port = process.env.PORT || 3000;
-app.listen(port);
+var server = app.listen(3000);
+var io = require('socket.io')(server);
 
-// // Render some console log output
-// console.log("Listening on port " + port);
+// Render some console log output
+console.log("Listening on port " + 3000);
 
-// var express = require("express");
-// var app = express();
-
-// // Set up a URL route
-// app.get("/", function(req, res) {
-//  res.sendFile(__dirname + '/index.html');
-// });
-
-// // bind the app to listen for connections on a specified port
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// var server = app.listen(port);
-// var io = require('socket.io')(server);
-
-// // Render some console log output
-// console.log("Listening on port " + port);
-
-// setInterval(function() {
-//       var i = Math.floor(Math.random() * 1000) + 1  
-//       //emit data on a websocket called mysocket
-//       io.sockets.emit('mysocket', i);
-//       console.log(i);
-// }, 10);
-
-
+setInterval(function() {
+    var i = Math.floor(Math.random() * 1000) + 1
+        //emit data on a websocket called mysocket
+    io.sockets.emit('mysocket', i);
+    console.log(i);
+}, 10);
 
 
 
